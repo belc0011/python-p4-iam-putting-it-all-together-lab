6aa2a7910caaf19c30fe4992f7c4fe8911079b96
Monica Garcia <monicairenegarcia@gmail.com>
2024-06-13 16:10:46 -0700

    first two test suites passing

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session, make_response
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 
@@ -8,10 +8,33 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    def post(self):
+        request_dict = request.get_json()
+        if 'username' not in request_dict or 'password' not in request_dict or 'image_url' not in request_dict or 'bio' not in request_dict:
+            return {'message': 'Missing necessary attributes'}, 422
+        else:
+            new_user = User(
+                username=request_dict['username'], 
+                image_url=request_dict['image_url'], 
+                bio=request_dict['bio'])
+            new_user.password_hash = request_dict['password']
+            
+            db.session.add(new_user)
+            db.session.commit()
+            new_user_dict = new_user.to_dict()
+            session['user_id'] = new_user.id
+            response = make_response(new_user_dict, 201)
+            return response
+        
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if session['user_id']:
+            user = User.query.filter_by(id=session['user_id']).first()
+            response = make_response(user.to_dict(), 200)
+            return response
+        else:
+            return {'message': 'Error, unauthorized user'}, 401
 
 class Login(Resource):
     pass
@@ -27,7 +50,6 @@ api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 api.add_resource(Login, '/login', endpoint='login')
 api.add_resource(Logout, '/logout', endpoint='logout')
 api.add_resource(RecipeIndex, '/recipes', endpoint='recipes')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import validates
+from sqlalchemy import CheckConstraint, UniqueConstraint
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
 
@@ -7,9 +8,54 @@ from config import db, bcrypt
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    pass
+    serialize_rules = ('-recipes.user',)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+    recipes = db.relationship('Recipe', back_populates="user")
+
+    __table_args__ = (
+        UniqueConstraint('username'),
+    )
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError("Password hash is not a readable attribute.")
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+    
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+    
+    @validates('username')
+    def validate_username(self, key, username):
+        if not username:
+            raise ValueError("Username is required.")
+        return username
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
     
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String, nullable=False)
+    minutes_to_complete = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    user = db.relationship('User', back_populates="recipes")
+    __table_args__ = (
+        CheckConstraint("length(instructions) >= 50", name='check_instructions_length'),
+    )
+
+    @validates('instructions')
+    def validate_instructions(self, key, instructions):
+        if len(instructions) < 50:
+            raise ValueError("Instructions must be at least 50 characters")
+        return instructions
